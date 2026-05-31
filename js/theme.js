e47f9b43cc12597ee21dc9e287ea2839e820215f
Muhamad Soleh <solehudinz3692@gmail.com>
2026-05-31 17:51:12 +0700

    Update theme.js

--- js/theme.js
+++ js/theme.js
@@ -373,5 +373,80 @@ form.on('submit', function (e) {
 		});
 	}
 });
+/*---------------------------------
+   TOGGLE DARK MODE NEW VERSION
+----------------------------------- */
+const toggleBtn = document.getElementById('darkModeToggle');
+const html = document.documentElement;
+
+function loadDarkCSS() {
+    if (!document.getElementById('dark-mode-css')) {
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = 'css/dark.css';
+        link.id = 'dark-mode-css';
+        document.head.appendChild(link);
+    }
+}
+
+function removeDarkCSS() {
+    const darkLink = document.getElementById('dark-mode-css');
+    if (darkLink) darkLink.remove();
+}
+
+if (localStorage.getItem('theme') === 'dark' || 
+   (!localStorage.getItem('theme') && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    html.classList.add('dark');
+    toggleBtn.classList.add('dark');
+    loadDarkCSS();
+}
+
+toggleBtn.addEventListener('click', () => {
+    html.classList.toggle('dark');
+    toggleBtn.classList.toggle('dark');
+
+    if (html.classList.contains('dark')) {
+        localStorage.setItem('theme', 'dark');
+        loadDarkCSS();
+    } else {
+        localStorage.setItem('theme', 'light');
+        removeDarkCSS();
+    }
+});
+
+/*---------------------------------
+   WIDGET FOR ADMIN WATSHAPP BRO
+----------------------------------- */
+$(document).on("click", "#send-it", function () {
+var a = document.getElementById("chat-input");
+if ("" != a.value) {
+var b = $("#get-number").text(),
+c = document.getElementById("chat-input").value,
+d = "https://web.whatsapp.com/send",
+e = b,
+f = "&text=" + c;
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))
+var d = "whatsapp://send";
+var g = d + "?phone=" + e + f;
+window.open(g, "_blank");
+a.value = "";
+}
+}),
+
+$(document).on("click", ".informasi", function () {
+document.getElementById("get-number").innerHTML = $(this).children(".my-number").text();
+$(".start-chat,.get-new").addClass("show").removeClass("hide");
+$(".home-chat,.head-home").addClass("hide").removeClass("show");
+document.getElementById("get-nama").innerHTML = $(this).children(".info-chat").children(".chat-nama").text();
+document.getElementById("get-label").innerHTML = $(this).children(".info-chat").children(".chat-label").text();
+}),
+
+$(document).on("click", ".close-chat", function () {
+$("#whatsapp-chat").addClass("hide").removeClass("show");
+}),
+
+$(document).on("click", ".msolehshow-chat", function () {
+$("#whatsapp-chat").addClass("show").removeClass("hide");
+});
 
 })(jQuery)
